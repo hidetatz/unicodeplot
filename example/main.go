a4bfe0c35da5de183389ed4da855f38a9377e513
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-07-24 08:31:42 +0900

    improve example code to show more available options

--- example/main.go
+++ example/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/hidetatz/unicodeplot"
 	"github.com/hidetatz/unicodeplot/barplot"
 )
 
 func main() {
 	if err := barplot.Print(
-		[]string{"tokyo", "sapporo"},
-		[]float64{123.1, 34.23},
-		barplot.WithTitle("temperature"),
+		[]string{"tokyo", "sapporo", "okinawa"},
+		[]float64{0.1, 3.23, 4.965321},
+		barplot.WithColor(unicodeplot.Magenta),
+		barplot.WithTitle("temp"),
+		barplot.WithBorder("barplot"),
+		barplot.WithMargin(5),
+		barplot.WithWidth(80),
+		barplot.WithShowBorder(false),
 	); err != nil {
 		fmt.Println(err)
 	}
